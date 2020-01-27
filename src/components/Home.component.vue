a21368686e85044c1d84e8d5b349937af5dd6695
Mike Mueller <muellerm.dev@gmail.com>
2020-01-27 14:18:55 -0500

    initial commit of framework

---
+++ src/components/Home.component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Home">
+    <h2>{msg}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
