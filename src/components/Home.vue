104d301ff5eece1b83a6307a228b36bf17b9c871
Usman Butt <usman.butt@dcmail.ca>
2020-02-03 16:51:47 -0500

    added class to router link

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,29 +1,45 @@
 <template>
-  <div id="Home">
-    <h2>LakeRidge Home</h2>
-    <router-link :to="{name: 'Browse', props: { } }">Browse Myths</router-link>
-    <router-link :to="{name: 'GameBox', props: { } }">Play Game</router-link>
-  </div>
+	<div id="Home">
+		<h2>LakeRidge Home</h2>
+		<router-link class="playButton button" :to="{name: 'GameBox', props: { } }">Play Game</router-link>
+		<br />
+		<router-link class="skipButton button" :to="{name: 'Browse', props: { } }">Browse Myths</router-link>
+	</div>
 </template>
 
 <script>
-import axios from "axios";
+	import axios from "axios";
 
-export default {
-  name: "Home",
-  props: {},
-  components: {},
-  data() {
-    return {
-      myths: null
-    };
-  },
-  created() {
-    axios.get("./data.json").then(response => (this.myths = response));
-  }
-};
+	export default {
+		name: "Home",
+		props: {},
+		components: {},
+		data() {
+			return {
+				myths: null
+			};
+		},
+		created() {
+			axios.get("./data.json").then(response => (this.myths = response));
+		}
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+	.playButton {
+		color: #ffffff;
+		background: #fa9500;
+		text-decoration: none;
+		/* padding: 1em; */
+	}
+
+	.skipButton {
+		color: #44494d;
+		/* background: #fff; */
+		text-decoration: none;
+		border: solid 3px #444a4d;
+		/* padding: 1em; */
+	}
 </style>
+
