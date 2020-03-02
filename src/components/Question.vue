d30b7deb39e7a009f3ea9d1ecbe648ef8b9e5afb
Usman Butt <usman.butt@dcmail.ca>
2020-03-01 23:35:52 -0500

    updated style in Question.vue

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -23,6 +23,9 @@
 				<div class="inner-shadow"></div>
 				<div class="screen">
 					<!-- Content goes here -->
+					<!-- <div class="bubble" v-for="(item) in mythData.bubbles" :key="item.id">
+						<p>{{item}}</p>
+					</div>-->
 					<div class="bubble" v-for="(item) in mythData.bubbles" :key="item.id">
 						<p>{{item}}</p>
 					</div>
@@ -30,11 +33,6 @@
 						<div class="nextBtn" v-on:click="nextEvent">Fact</div>
 						<div class="nextBtn" v-on:click="nextEvent">Myth</div>
 					</div>
-
-					<section class="speech-bubble">
-						<h4>{{mythData.myth_title}}</h4>
-					</section>
-					<button class="true" v-on:click="nextEvent">True</button>
 				</div>
 			</div>
 		</section>
@@ -49,26 +47,6 @@
 			nextEvent: Function
 		}
 	};
-	//   <div class="Question">
-	//     <div class="bubble" v-for="(item) in mythData.bubbles" :key="item.id">
-	//       <p>{{item}}</p>
-	//     </div>
-	//     <div class="choices">
-	//       <div class="nextBtn" v-on:click="nextEvent">Fact</div>
-	//       <div class="nextBtn" v-on:click="nextEvent">Myth</div>
-	//     </div>
-	//   </div>
-	// </template>
-
-	// <script>
-	// export default {
-	//   name: "Question",
-	//   props: {
-	//     mythData: Object,
-	//     nextEvent: Function,
-	//     testData: Object
-	//   }
-	// };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -79,7 +57,7 @@
 		display: flex;
 		flex-direction: row;
 		justify-content: center;
-		margin-top: -1vh;
+		/* margin-top: -1vh; */
 		animation: appear 0.8s ease-out;
 	}
 	.logo {
@@ -90,61 +68,28 @@
 	.screen {
 		padding: 5vh;
 	}
-	.speech-bubble {
-		position: relative;
-		background: #c7c8ca;
-		border-radius: 0.8rem;
+
+	p {
+		padding: 1rem;
+		color: #fff;
+		/* width: 70%; */
+		height: auto;
+		border-radius: 3.5rem;
 		padding: 3rem;
+		margin: 1rem 0;
 		font-size: 2rem;
 		text-align: left;
 		line-height: 2.15rem;
 	}
 
-	.speech-bubble:after {
-		content: "";
-		position: absolute;
-		left: 0;
-		top: 50%;
-		width: 0;
-		height: 0;
-		border: 2rem solid transparent;
-		border-right-color: #c7c8ca;
-		border-left: 0;
-		border-top: 0;
-		margin-top: -4rem;
-		margin-left: -1em;
-	}
-	.true {
-		padding: 1rem 0 0 3rem;
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		flex-wrap: wrap;
-	}
-	/* .Question {
-				width: 25vw;
-				height: 500px;
-				background: #111;
-				display: flex;
-				flex-direction: column;
-				justify-content: space-around;
-				align-items: center;
-				animation: appear 0.8s ease-out;
-			} */
-
-	p {
-		padding: 1rem;
-		color: #fff;
-		width: 70%;
-		height: auto;
-	}
-
 	.bubble:nth-child(even) p {
-		background-color: rgb(92, 145, 161);
+		/* background-color: rgb(92, 145, 161); */
+		background-color: #27aae1;
 	}
 
 	.bubble:nth-child(odd) p {
-		background-color: #aaa;
+		/* background-color: #aaa; */
+		background-color: #c7c8ca;
 	}
 
 	.nextBtn {
@@ -155,6 +100,7 @@
 		color: #fff;
 		background-color: rgb(92, 145, 161);
 		cursor: pointer;
+		border-radius: 10rem;
 	}
 
 	.choices {
