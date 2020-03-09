11e831b3b5caf0ef3451c06e9d560f62fb0ab3b8
Michael Mueller <muellerm.dev@gmail.com>
2020-03-09 16:41:19 -0400

    resrouces now functional

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="Resources">
-    <h4>Resources</h4>
-    <div class="bubble" v-for="(item) in this.resources.data.resources.res" :key="item.id">
-      <h2>{{item.title}}</h2>
-      <a :href="item.href[0]"></a>
-      <a :href="item.href[1]"></a>
+    <div class="Resource-cards">
+      <div class="resource-card" v-for="(item) in this.resources" :key="item.id">
+        <h2>{{item.res[0].title}}</h2>
+        <div class="links">
+          <a :href="item.res[0].href">{{item.res[0].href}}</a>
+          <a :href="item.res[1].href">{{item.res[1].href}}</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,7 +23,9 @@ export default {
     };
   },
   created() {
-    axios.get("./data.json").then(response => (this.resources = response));
+    axios
+      .get("./data.json")
+      .then(response => (this.resources = response.data.resources));
   }
 };
 </script>
@@ -31,6 +36,43 @@ export default {
   border-radius: 10px;
   width: 320px;
   height: 90vh;
-  background: red;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: flex-start;
+  /* background: red; */
+}
+
+.Resource-cards {
+  width: 100vw;
+  height: 80vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: flex-start;
+}
+
+.resource-card {
+  width: 15vw;
+  height: 26rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgb(211, 211, 211);
+}
+
+.links {
+  padding: 1rem;
+  width: 60%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.links a {
+  padding: 2rem 0;
 }
 </style>
