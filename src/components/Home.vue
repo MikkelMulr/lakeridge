6403d5f32eabd2451fcfff47efe7d3ced5de9277
Usman Butt <usman.butt@dcmail.ca>
2020-03-09 16:01:57 -0400

    updated style to inculde the white box to work with blue header and footer

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,89 +1,102 @@
 <template>
-  <main id="home">
-    <section class="home-headings">
-      <p class="home-para">Know the facts behind</p>
-      <p>the common myths of</p>
-      <h1>Clinical Trials</h1>
-    </section>
+	<main id="home">
+		<section class="home-headings">
+			<section class="line">
+				<img src="../assets/images/line.svg" alt="line" />
+			</section>
+			<p class="home-para">Know the facts behind</p>
+			<p>the common myths of</p>
+			<h1>Clinical Trials</h1>
+		</section>
 
-    <section class="play">
-      <router-link class="playButton button" :to="{name: 'GameBox', props: { } }">Let's Go</router-link>
-    </section>
-    <section class="hex"></section>
-    <footer>LakeRidge Health 2019</footer>
-  </main>
+		<section class="play">
+			<router-link class="playButton button" :to="{name: 'GameBox', props: { } }">Let's Go</router-link>
+		</section>
+	</main>
 </template>
 
 <script>
-export default {
-  name: "Home",
-  props: {},
-  components: {}
-};
+	export default {
+		name: "Home",
+		props: {},
+		components: {}
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#home {
-  background: #c7c8ca;
-  width: 100vw;
-  height: 90vh;
-  max-height: 100vh;
-  overflow: hidden;
-}
+	#home {
+		width: 95vw;
+		height: 80vh;
+		background: #f5f5f5;
+		margin: 2rem auto 0 auto;
+		overflow: hidden;
+		border-radius: 0.5rem;
+		min-height: 83vh;
+	}
+	.line {
+		display: flex;
+		position: absolute;
+		width: 100%;
+		justify-content: center;
+		text-align: center;
+		padding-top: 1rem;
+		padding-right: 55rem;
+		height: 100px;
+	}
 
-.home-headings {
-  padding: 6rem 0;
-}
+	.home-headings {
+		padding: 15rem 0 10rem 0;
+	}
 
-h1 {
-  align-items: center;
-  font-size: 7rem;
-  font-weight: 400;
-  text-align: center;
-  line-height: 8rem;
-}
-p {
-  font-size: 4rem;
-  font-weight: 400;
-}
+	h1 {
+		align-items: center;
+		font-size: 8rem;
+		font-weight: 400;
+		text-align: center;
+		line-height: 8rem;
+		padding-top: 0.95rem;
+	}
+	p {
+		font-size: 4rem;
+		font-weight: 400;
+	}
 
-.play {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-}
+	.play {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
 
-.playButton {
-  height: 12rem;
-  width: 55rem;
-  font-size: 5rem;
-  text-transform: uppercase;
-  border-radius: 0.75rem;
-  color: #fff;
-  background: #fa9500;
-  text-decoration: none;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  font-weight: 700;
-  box-shadow: 0.1rem 0.1rem 0.3rem #242323;
-}
+	.playButton {
+		height: 12rem;
+		width: 55rem;
+		font-size: 5rem;
+		text-transform: uppercase;
+		border-radius: 0.75rem;
+		color: #fff;
+		background: #fa9500;
+		text-decoration: none;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		font-weight: 700;
+		box-shadow: 0.1rem 0.1rem 0.3rem #242323;
+	}
 
-.playButton:hover {
-  box-shadow: none;
-}
-
-.hex {
-  /* position: absolute; */
-  margin-left: -10rem;
-  margin-top: 32rem;
-  width: 20rem;
-  height: 20rem;
-  background: rgba(255, 255, 255, 0.308);
-  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
-}
+	.playButton:hover {
+		box-shadow: none;
+	}
+	/*
+																																								.hex {
+																																									margin-left: -10rem;
+																																									margin-top: 32rem;
+																																									width: 20rem;
+																																									height: 20rem;
+																																									background: rgba(255, 255, 255, 0.308);
+																																									clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+																																								} */
 </style>
 
