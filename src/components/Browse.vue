4837747c48938c53c1af10f25d3b946e4b179e1e
Usman Butt <usman.butt@dcmail.ca>
2020-03-23 15:12:21 -0400

    updated code from GitHub to match master branch

--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -1,38 +1,39 @@
 <template>
-  <div id="Browse">
-    <QuestionList :mythData="this.myths" />
-  </div>
+	<div id="Browse">
+		<QuestionList :mythData="this.myths" />
+	</div>
 </template>
 
 <script>
-import QuestionList from "./QuestionList";
-import axios from "axios";
+	import QuestionList from "./QuestionList";
+	import axios from "axios";
 
-export default {
-  name: "Browse",
-  props: {},
-  components: {
-    QuestionList
-  },
-  data() {
-    return {
-      myths: {}
-    };
-  },
-  created() {
-    axios.get("./data.json").then(response => (this.myths = response));
-  }
-};
+	export default {
+		name: "Browse",
+		props: {},
+		components: {
+			QuestionList
+		},
+		data() {
+			return {
+				myths: {}
+			};
+		},
+		created() {
+			axios.get("./data.json").then(response => (this.myths = response));
+		}
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#Browse {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-}
+	#Browse {
+		width: 100vw;
+		height: 100vh;
+		padding: 2rem 10rem;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
 </style>
