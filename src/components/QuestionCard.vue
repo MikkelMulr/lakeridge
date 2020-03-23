31e32e34ecad73b766649437f885e714abbea015
Usman Butt <usman.butt@dcmail.ca>
2020-03-23 15:16:32 -0400

    updated code from GitHub to match master branch

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -1,26 +1,41 @@
 <template>
-  <div class="QuestionCard" v-on:click="handleClick(id - 1)">
-    <h4>{{myth}}</h4>
-  </div>
+	<div class="QuestionCard" v-on:click="handleClick(id - 1)">
+		<h4>{{myth}}</h4>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "QuestionCard",
-  props: {
-    myth: String,
-    id: Number,
-    handleClick: Function
-  }
-};
+	export default {
+		name: "QuestionCard",
+		props: {
+			myth: String,
+			id: Number,
+			handleClick: Function
+		}
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.QuestionCard {
-  border-radius: 10px;
-  width: 320px;
-  height: 150px;
-  background: red;
-}
+	.QuestionCard {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		border-radius: 10px;
+		width: 70vw;
+		height: 85px;
+		background: #ddd;
+		margin: 2rem 0;
+		font-size: 2rem;
+		transition: all 0.35s linear;
+		cursor: pointer;
+	}
+
+	.QuestionCard:hover {
+		transform: scale(1.075);
+		background-color: #27aae1;
+		color: #fff;
+		box-shadow: 0.1rem 0.1rem 0.9rem 0.5rem #eeeeee44;
+	}
 </style>
--- src/components/QuestionList.vue
+++ src/components/QuestionList.vue
@@ -1,66 +1,66 @@
 <template>
-  <div id="QuestionList">
-    <div class="CardView" v-if="this.viewState">
-      <Information
-        :mythData="mythData.data.myths[this.mythClicked]"
-        :returnTo="updateViewState"
-        :viewType="'browse'"
-      />
-    </div>
-    <div class="CardView" v-else>
-      <QuestionCard
-        v-for="(item) in mythData.data.myths"
-        :key="item.id"
-        :id="item.id"
-        :myth="item.myth_title"
-        :handleClick="updateMythClicked"
-      />
-      <router-link :to="{name: 'Home', props: { } }">Return to menu</router-link>
-    </div>
-  </div>
+	<div id="QuestionList">
+		<div class="CardView" v-if="this.viewState">
+			<Information
+				:mythData="mythData.data.myths[this.mythClicked]"
+				:returnTo="updateViewState"
+				:viewType="'browse'"
+			/>
+		</div>
+		<div class="CardView" v-else>
+			<QuestionCard
+				v-for="(item) in mythData.data.myths"
+				:key="item.id"
+				:id="item.id"
+				:myth="item.myth_title"
+				:handleClick="updateMythClicked"
+			/>
+			<!-- <router-link :to="{name: 'Home', props: { } }">Return to menu</router-link> -->
+		</div>
+	</div>
 </template>
 
 <script>
-import QuestionCard from "./QuestionCard.vue";
-import Information from "./Information";
-// v-on:click="updateMythClicked(item.id)"
+	import QuestionCard from "./QuestionCard.vue";
+	import Information from "./Information";
+	// v-on:click="updateMythClicked(item.id)"
 
-export default {
-  name: "QuestionList",
-  props: {
-    mythData: Object
-  },
-  data() {
-    return {
-      viewState: false,
-      mythClicked: 0
-    };
-  },
-  components: {
-    QuestionCard,
-    Information
-  },
-  methods: {
-    updateViewState: function() {
-      this.viewState = !this.viewState;
-    },
-    updateMythClicked: function(id) {
-      this.mythClicked = id;
-      this.updateViewState();
-    }
-  }
-};
+	export default {
+		name: "QuestionList",
+		props: {
+			mythData: Object
+		},
+		data() {
+			return {
+				viewState: false,
+				mythClicked: 0
+			};
+		},
+		components: {
+			QuestionCard,
+			Information
+		},
+		methods: {
+			updateViewState: function() {
+				this.viewState = !this.viewState;
+			},
+			updateMythClicked: function(id) {
+				this.mythClicked = id;
+				this.updateViewState();
+			}
+		}
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#QuestionList {
-  flex-wrap: wrap;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  height: 100%;
-  width: 100%;
-}
+	#QuestionList {
+		flex-wrap: wrap;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		align-items: flex-start;
+		height: 100%;
+		width: 100%;
+	}
 </style>
